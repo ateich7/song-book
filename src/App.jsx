64a141f7461cc7360a0ab8ec817eb41cea7e60b8
ateich7 <140982241+ateich7@users.noreply.github.com>
2025-05-19 17:03:30 -0400

    Create non-functional, lo-fi UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,67 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { songs } from './songs.js'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-full mx-8 mt-8 flex flex-col gap-6">
+      <div className="flex flex-row justify-between">
+        <h1 className="text-4xl font-medium">Songs</h1>
+        <div className="flex gap-6">
+          <Button variant="outline">Write Song</Button>
+          <Button variant="outline">Add Song</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Input placeholder="Search" />
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="all">All</TabsTrigger>
+          <TabsTrigger value="mem">Memorize</TabsTrigger>
+          <TabsTrigger value="jam">Jam</TabsTrigger>
+          <TabsTrigger value="ogl">Originals</TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Title</TableHead>
+                <TableHead>Artist</TableHead>
+                <TableHead>Capo</TableHead>
+                <TableHead className="text-right">Last Played</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TabsContent>
+        <TabsContent value="mem">memorize</TabsContent>
+        <TabsContent value="jam">jam</TabsContent>
+        <TabsContent value="ogl">originals</TabsContent>
+      </Tabs>
+
+    </div>
   )
 }
 
---
+++ src/songs.js
@@ -0,0 +1,20 @@
+export const songs = [
+    {
+      title: "Yellow",
+      artist: "Coldplay",
+      capo: 2,
+      lastOpened: "2025-05-18",
+    },
+    {
+      title: "Shallow",
+      artist: "Lady Gaga & Bradley Cooper",
+      capo: 3,
+      lastOpened: "2025-05-15",
+    },
+    {
+      title: "Thinking Out Loud",
+      artist: "Ed Sheeran",
+      capo: 1,
+      lastOpened: "2025-05-10",
+    },
+  ];
